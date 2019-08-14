69853a2400b45b39b7e232c568989cc51c5c5ab7
msviha <msviha@gmail.com>
2019-08-15 00:50:36 +0200

    init

---
+++ client/src/app/chat/chat.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import * as Colyseus from 'colyseus.js';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.sass']
+})
+export class ChatComponent implements OnInit {
+
+  inputVal = 'asd';
+
+  room;
+
+  messages:string[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    const client = new Colyseus.Client('ws://localhost:4201');
+    this.room = client.join('chat');
+    
+    this.room.listen('messages/:index', (change) => {
+        console.log('change: ', change);
+    });
+
+    this.room.onJoin.add(() => {
+      console.log('joined');
+    });
+
+    this.room.onStateChange.addOnce((state) => {
+      console.log('initial room state: ', state);
+    });
+
+    // new room state
+    this.room.onStateChange.add((state) => {
+      // this signal is triggered on each patch
+      console.log('stateChange triggers: ', state);
+    });
+
+    // listen to patches coming from the server
+    this.room.onMessage.add((message) => {
+      this.messages.push(message);
+    });
+
+  }
+
+  send() {
+    console.log('input:', this.inputVal);
+    this.room.send({message: this.inputVal});
+    this.inputVal = '';
+  }
+
+}
